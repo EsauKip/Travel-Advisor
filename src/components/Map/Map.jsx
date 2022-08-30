8671c2e27437a49f213d7f98fe11924a6484be7a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-30 12:12:13 +0300

    add imports and update the styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import googleMapReact from 'google-map-react'
+import{Paper, Typogrephy, useMediaQuery} from '@material-ui/core'
+import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+import useStyles from './styles'
 const Map = () => {
   return (
     <div>Map</div>
